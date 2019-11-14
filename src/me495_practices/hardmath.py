f8da0adca0ac82de32f4bed83582e6f4e91afdce
Matthew Elwin <elwin@northwestern.edu>
2019-11-14 00:38:33 -0600

    Working on services.

---
+++ src/me495_practices/hardmath.py
@@ -0,0 +1,12 @@
+""" Some complicated mathematical functions """
+
+
+def add_two(a, b):
+    """ Adds two numbers together and returns the sum
+    ARGS:
+       a: a number
+       b: a number
+    RETURN:
+       a + b
+    """
+    return a + b
