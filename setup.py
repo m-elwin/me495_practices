476a1fc5dbd81bc3d5f82f1c93d80f49b5a34163
Matthew Elwin <elwin@northwestern.edu>
2020-09-30 21:15:24 -0500

    Updated with python3 setup.py
    The setup.py file now uses setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # Python setup file used for installing a python catkin package.
 # See http://docs.ros.org/melodic/api/catkin/html/user_guide/setup_dot_py.html
 # For catkin to use this we have called python_catkin_setup() in CMakeLists.txt
-from distutils.core import setup
+from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
